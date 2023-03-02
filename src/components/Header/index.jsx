0dc99d55bcbbad77f275d3bd2f8a0a0769737ea6
asimenstad <a-nna-s@hotmail.com>
2023-03-02 13:12:48 +0100

    Install React Router

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
