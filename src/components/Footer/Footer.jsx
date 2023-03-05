a0187dacafdaf238cfc5f2aab71b97f61fd02738
asimenstad <a-nna-s@hotmail.com>
2023-03-05 17:32:04 +0100

    Created footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import * as S from "./Footer.styled";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <S.Footer>
+      <S.FooterWrapper>
+        <div>Logo etc</div>
+        <div>Social media</div>
+      </S.FooterWrapper>
+    </S.Footer>
+  );
 }
 
 export default Footer;
---
+++ src/components/Footer/Footer.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+`;
+
+export const FooterWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1200px;
+  justify-content: space-between;
+`;
